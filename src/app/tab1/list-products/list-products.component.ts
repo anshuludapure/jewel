83bab51e1dea04eac442b93f68427581a8cc9046
unknown <anshul.udapure@gmail.com>
2024-03-07 14:37:28 +0530

    search functionality

---
+++ src/app/tab1/list-products/list-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-products',
+  templateUrl: './list-products.component.html',
+  styleUrls: ['./list-products.component.scss'],
+})
+export class ListProductsComponent  implements OnInit {
+
+  @Input() products: any;
+  constructor() { }
+
+  ngOnInit() {}
+
+}
